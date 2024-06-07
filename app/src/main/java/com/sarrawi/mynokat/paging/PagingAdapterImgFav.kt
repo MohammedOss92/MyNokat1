b578d75c7b2e9a523c21b879740f0fc1e9104f91
MohammedOss <abdallahomar1992@gmail.com>
2024-06-08 00:05:29 +0300

    aa

--- app/src/main/java/com/sarrawi/mynokat/paging/PagingAdapterImgFav.kt
+++ app/src/main/java/com/sarrawi/mynokat/paging/PagingAdapterImgFav.kt
@@ -1,6 +1,7 @@
 package com.sarrawi.mynokat.paging
 
 import android.content.Context
+import android.graphics.drawable.BitmapDrawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,6 +14,8 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DecodeFormat
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
+import com.sarrawi.img.utils.SaveImg
+import com.sarrawi.img.utils.Utils
 import com.sarrawi.mynokat.R
 import com.sarrawi.mynokat.databinding.ImgRowBinding
 import com.sarrawi.mynokat.model.FavImgModel
@@ -23,7 +26,7 @@ import com.sarrawi.mynokat.ui.frag.img.ImgFragmentDirections
 class PagingAdapterImgFav (val con: Context, val frag: Fragment) : PagingDataAdapter<FavImgModel, PagingAdapterImgFav.ViewHolder>(COMPARATOR) {
 
     var onbtnclick: ((Int,item:FavImgModel,Int) -> Unit)? = null
-
+    private var currentFlippedPosition: Int? = null
     inner class ViewHolder(private val binding: ImgRowBinding):RecyclerView.ViewHolder(binding.root) {
 
         init {
@@ -35,6 +38,8 @@ class PagingAdapterImgFav (val con: Context, val frag: Fragment) : PagingDataAda
                     }
                 }
             }
+
+            setupListeners()
         }
 
         fun bind(favImgModel: FavImgModel?){
@@ -58,7 +63,10 @@ class PagingAdapterImgFav (val con: Context, val frag: Fragment) : PagingDataAda
                     val directions = FavFragmentDirections.actionFavFragment2ToFavFragmentFull(it)
                     frag.findNavController().navigate(directions)
                 }
+
             }
+
+
 
             val requestOptions = RequestOptions()
                 .placeholder(R.drawable.ic_baseline_autorenew_24)
@@ -72,9 +80,94 @@ class PagingAdapterImgFav (val con: Context, val frag: Fragment) : PagingDataAda
                 .load(favImgModel?.image_url)
                 .apply(requestOptions)
                 .into(binding.imageView)
+
+
+        }
+        private fun setupListeners() {
+            binding.root.setOnLongClickListener {
+                handleItemClick(bindingAdapterPosition)
+                true
+            }
+
+            binding.imageView.setOnLongClickListener {
+                handleItemClick(bindingAdapterPosition)
+                true
+            }
+
+            binding.roundBtn.setOnClickListener {
+                handleItemClick(bindingAdapterPosition)
+                true
+            }
+
+            binding.btncmessenger.setOnClickListener {
+                Utils.shareImgMessenger(con,binding.imageView,binding.root)
+            }
+            binding.btnwhats.setOnClickListener {
+                Utils.shareImageWhatsApp(con,binding.imageView,"",binding.root)
+            }
+            binding.btncshare.setOnClickListener {
+                Utils.ImgShare(con,binding.imageView,binding.root)
+            }
+            binding.btnSave.setOnClickListener {
+                SaveImg.saveBitmapToExternalStorage(con,(binding.imageView.drawable as BitmapDrawable).bitmap)
+            }
+
         }
 
 
+        private fun handleItemClick(position: Int) {
+            val previousPosition = currentFlippedPosition
+
+            if (previousPosition != null && previousPosition != position) {
+                resetItemState(previousPosition)
+            }
+
+            currentFlippedPosition = position
+            val randomNumber = (1..2).random()
+            val imageUrl = getItem(position)?.image_url
+            flipTheCoin(imageUrl ?: "button", if (randomNumber == 1) "" else "")
+        }
+
+        private fun resetItemState(position: Int) {
+//            val holder = binding.imageView.findViewHolderForAdapterPosition(position) as? ViewHolder
+//            holder?.resetView()
+
+            val viewHolder = (frag.requireView().findViewById(R.id.rc_Img_Nokat) as RecyclerView).findViewHolderForAdapterPosition(position) as? ViewHolder
+            viewHolder?.resetView()
+        }
+
+        fun flipTheCoin(imageId: String, coinSide: String) {
+            binding.root.animate().apply {
+                duration = 1000
+                rotationYBy(360f)
+                binding.imageView.isClickable = false
+            }.withEndAction {
+                if (binding.imageView.visibility == View.VISIBLE) {
+                    binding.imageView.visibility = View.GONE
+                    binding.btnSave.visibility = View.VISIBLE
+                    binding.btncshare.visibility = View.VISIBLE
+                    binding.btncmessenger.visibility = View.VISIBLE
+                    binding.btnwhats.visibility = View.VISIBLE
+                } else {
+                    binding.imageView.visibility = View.VISIBLE
+                    binding.btncmessenger.visibility = View.GONE
+                    binding.btnwhats.visibility = View.GONE
+                    binding.btnSave.visibility = View.GONE
+                    binding.btncshare.visibility = View.GONE
+                }
+//                Toast.makeText(con, coinSide, Toast.LENGTH_SHORT).show()
+                binding.imageView.isClickable = true
+            }.start()
+        }
+
+        fun resetView() {
+            binding.root.clearAnimation()
+            binding.imageView.visibility = View.VISIBLE
+            binding.btncshare.visibility = View.GONE
+            binding.btnSave.visibility = View.GONE
+            binding.btnwhats.visibility = View.GONE
+            binding.btncmessenger.visibility = View.GONE
+        }
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
